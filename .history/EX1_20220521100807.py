dcad05d804bb35a87aaead4d2dcd3bf928953328
HoangMoon <hoangmoon8321@gmail.com>
2022-05-21 10:08:59 +0700

    fix lỗi ko mở dc file

---
+++ .history/EX1_20220521100807.py
@@ -0,0 +1,42 @@
+
+from turtle import Screen, left
+import pygame
+from pygame import mixer
+from pyparsing import White
+pygame.init()
+
+WIDTH = 1400
+HEIGHT = 800
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128,)
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Beat Maker of Moon')
+label_front = pygame.font.Font('freesansbold.ttf', 32)
+
+fps = 60
+timer= pygame.time.Clock()
+
+def draw_grid():
+    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT], 5)
+    bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT- 200, WIDTH, 200], 5)
+
+
+
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill(black)
+    draw_grid()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
+
--- EX1.py
+++ EX1.py
@@ -1,5 +1,5 @@
-from cProfile import label
-from turtle import Screen
+
+from turtle import Screen, left
 import pygame
 from pygame import mixer
 from pyparsing import White
@@ -18,13 +18,21 @@ label_front = pygame.font.Font('freesansbold.ttf', 32)
 
 fps = 60
 timer= pygame.time.Clock()
- 
+
+def draw_grid():
+    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT], 5)
+    bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT- 200, WIDTH, 200], 5)
+
+
+
+
 run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
+    draw_grid()
 
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
